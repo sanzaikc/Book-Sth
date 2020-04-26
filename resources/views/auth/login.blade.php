2051fc3cf5ec68ccc987fcef30690e262e9db394
JemyJenesh <xemejenesh@gmail.com>
2020-04-26 20:04:22 +0545

    shadow in the form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="container mx-auto">
-    <div class="mx-auto bg-white shadow-sm rounded-sm border-t-4 border-blue-400" style="width: 320px">
+    <div class="mx-auto bg-white shadow rounded-sm border-t-4 border-blue-400" style="width: 320px">
         <h1 class="mx-4 mt-6 font-bold text-2xl">Login</h1>
         <form method="POST" action="{{ route('login') }}" class="px-4 py-6">
             @csrf
             <div class="mb-4">
                 <label for="email" class="block text-gray-700 text-sm font-bold mb-2">{{ __('E-Mail Address') }}</label>
 
-                    <input id="email" type="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white @error('email') border-red-500 @else focus:border-blue-500 @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                    <input id="email" type="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full p-2 text-gray-700 leading-tight focus:outline-none focus:bg-white @error('email') border-red-500 @else focus:border-blue-500 @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
                     @error('email')
                         <p class="text-red-500 text-xs italic">{{ $message }}</p>
                     @enderror
@@ -18,7 +18,7 @@
             <div class="mb-4">
                 <label for="password" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Password') }}</label>
 
-                <input id="password" type="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white @error('password') border-red-500 @else focus:border-blue-500 @enderror" name="password" autocomplete="current-password">
+                <input id="password" type="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full p-2 text-gray-700 leading-tight focus:outline-none focus:bg-white @error('password') border-red-500 @else focus:border-blue-500 @enderror" name="password" autocomplete="current-password">
 
                 @error('password')
                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
@@ -45,6 +45,7 @@
             </div>
 
         </form>
+    </div>
 </div>
 
 @endsection
